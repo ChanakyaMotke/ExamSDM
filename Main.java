697789e9e6cc1b0110ddca41f134ddccdf814fa9
ChanakyaMotke <140472043+ChanakyaMotke@users.noreply.github.com>
2023-07-25 09:11:00 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,33 +1,27 @@
 package sdm;
-
-
 import java.util.Scanner;
 
 public class Main {
-  
-      public static void main(String[] args) {
-    	        @SuppressWarnings("resource")
-				Scanner sc = new Scanner(System.in);
-
-    	        System.out.print("Enter the first number: ");
-    	        int num1 = sc.nextInt();
-
-    	        System.out.print("Enter the second number: ");
-    	        int num2 = sc.nextInt();
-
-    	        System.out.print("Enter the third number: ");
-    	        int num3 = sc.nextInt();
-
-    	        int sum = 0;
-
-    	        if (num1 >= num2 && num1 >= num3) {
-    	            sum = num1;
-    	        } else if (num2 >= num1 && num2 >= num3) {
-    	            sum = num2;
-    	        } else {
-    	            sum = num3;
-    	        }
-
-    	        System.out.println("The sum of the largest number is: " + sum);
-    	    }
-    	}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        
+        System.out.print("Enter the third number: ");
+        double num3 = scanner.nextDouble();
+        
+        double sum = num1 + num2 + num3;
+        double average = sum / 3;
+        
+        double largest = Math.max(num1, Math.max(num2, num3));
+        
+        System.out.println("Sum of the largest number: " + largest);
+        System.out.println("Average of the three numbers: " + average);
+        
+        scanner.close();
+    }
+}
